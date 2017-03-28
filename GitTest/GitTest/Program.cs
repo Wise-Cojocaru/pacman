d280d4537f7e40ce29a298af0b3631d1faadf604
cwise827 <cwise827@students.bju.edu>
2017-03-28 14:41:32 -0400

    no O

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             //Hi	
-		// Yo yO
+		// Yo y
         }
     }
 }
