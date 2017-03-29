2bd1bc0318f4da5f8663fe52f794c4c33714e889
gcojo543 <gcojo543@students.bju.edu>
2017-03-29 09:35:41 -0400

    Created the project and added some classes

---
+++ PacMan/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PacMan.Models;
+namespace PacMan
+{
+    class GameController
+    {
+        public List<Map> Maps = new List<Map>();
+        
+
+        public GameController()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
